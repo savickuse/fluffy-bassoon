d2ccc567b6f17c3e3550bd91aa60190bb3a0e0d8
savickuse <savickuse@users.noreply.github.com>
2025-09-15 19:51:19 +0300

    feat(config): improve user feedback

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 36
 module.exports = API;
+
+
+// API module for BassoonSync
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 51
+module.exports = API;
