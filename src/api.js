2a83498c350b11d3c4632aa0c3df9f24d5484fb9
savickuse <savickuse@users.noreply.github.com>
2025-09-16 04:45:28 +0300

    refactor: add feature flag

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// API module for BassoonSync
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 1
+module.exports = API;
