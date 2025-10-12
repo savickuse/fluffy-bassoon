a436563d82466f4bd7051ffc5f3e77c0b84ae6e9
savickuse <savickuse@users.noreply.github.com>
2025-10-12 19:12:48 +0300

    perf: refactor module structure

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 9
 module.exports = API;
+
+
+// API module for BassoonSync
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 15
+module.exports = API;
