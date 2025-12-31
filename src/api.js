9d38e3fe98b29762994d5bab45c68f82899e079a
savickuse <savickuse@users.noreply.github.com>
2025-12-31 22:27:03 +0200

    remove: fix security vulnerability

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 15
 module.exports = API;
+
+
+// API module for BassoonSync
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 33
+module.exports = API;
